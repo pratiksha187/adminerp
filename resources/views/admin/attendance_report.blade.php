6657987e9f1d0233a1f7ddbf076ce3aaf70298b9
pratiksha187 <pratikshanmisal@gmail.com>
2026-03-28 16:46:54 +0530

    chnages

--- resources/views/admin/attendance_report.blade.php
+++ resources/views/admin/attendance_report.blade.php
@@ -48,6 +48,7 @@
         </div>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script>
